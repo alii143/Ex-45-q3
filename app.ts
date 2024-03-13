8af01346d6fd8009b4a48eb25e5f0ae62b375dcb
Waqas Ali <160438548+alii143@users.noreply.github.com>
2024-03-14 04:26:14 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+// lovercase
+let personName: string= "Ali"
+console.log("lowercase:", personName.toLowerCase())
+
+// upper case
+console.log("uppercase:", personName.toUpperCase());
+
+// title case
+console.log("titlecase:", personName.replace(/\bw/g,c=> c.toUpperCase()));
